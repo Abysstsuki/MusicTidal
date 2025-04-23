2bf7a2f2ef63db49dd1b7fe9f711030f6d58d8a4
Abysstsuki <927633106@qq.com>
2025-04-23 23:10:04 +0800

    添加bg，初始化musicplayer

---
+++ frontend/src/components/musicplayer.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+
+export default function MusicPlayer(){
+    return(
+        <Fragment>
+            <main className="w-[40vh] h-[40vh] bg-red-600">
+
+            </main>
+        </Fragment>
+    )
+}
