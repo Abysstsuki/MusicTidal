994ef15cff773de0465b9d7eefbddf4d96903b62
Abysstsuki <927633106@qq.com>
2025-04-24 23:33:35 +0800

    样式更改

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Fragment } from "react";
 export default function Home() {
   return (
     <Fragment>
-      <div className="">
+      <div className="fixed top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <MusicPlayer></MusicPlayer>
         </div>
     </Fragment>
